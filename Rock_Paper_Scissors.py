3db51ab994515f3d145d674601b74a6dcca96b4f
Patrick <koppatrick@yahoo.com>
2022-07-17 22:34:53 -0400

    first commit

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,33 @@
+from random import randint
+
+RPS = ["Rock", "Paper", "Scissors"]
+
+computer = RPS[randint(0, 2)]
+
+player = False
+
+while player == False:
+
+    player = input("Rock, Paper, Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+
+    player = False  # Allows the game to loop
+    computer = RPS[randint(0, 2)]
